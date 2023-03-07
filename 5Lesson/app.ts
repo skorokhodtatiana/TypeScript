23e85c1ad3bfdec56d324cdb9e96d09484fdb961
tskorohod <tskorohod@lmteam.by>
2023-03-07 22:41:09 +0300

    hw 5 lesson, generics

---
+++ 5Lesson/app.ts
@@ -0,0 +1,12 @@
+//Написать дженерик функцию с одним параметром и с её помощью вывести в консоль сумму чисел,
+//если параметры типа number и приветствие, если параметры типа string или boolean.
+function echo <T>(val: T): T {
+	if (typeof val === 'number') {
+		console.log(val);
+		return val;
+	} else {
+		console.log('Hy ' + val)
+	}
+}
+
+echo("nnnn");
