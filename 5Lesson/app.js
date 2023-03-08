4fcba619d4b70e7abc8575eae1506c945c984de7
tskorohod <tskorohod@lmteam.by>
2023-03-08 21:50:14 +0300

    generic types

--- 5Lesson/app.js
+++ 5Lesson/app.js
@@ -7,6 +7,21 @@ function echo(val) {
     }
     else {
         console.log('Hy ' + val);
+        return val;
     }
 }
 echo("nnnn");
+//Создать обобщённый класс с тремя параметрами (T, V, K).
+var Person = /** @class */ (function () {
+    function Person(name, login, password) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+    Person.prototype.createAccount = function () {
+        console.log("Hi, ".concat(this.name));
+    };
+    return Person;
+}());
+var NewPerson = new Person('Tanja', 'login', 'password');
+NewPerson.createAccount();
--- 5Lesson/app.ts
+++ 5Lesson/app.ts
@@ -6,7 +6,30 @@ function echo <T>(val: T): T {
 		return val;
 	} else {
 		console.log('Hy ' + val)
+		return val;
 	}
 }
 
 echo("nnnn");
+
+//Создать обобщённый класс с тремя параметрами (T, V, K).
+class Person <T, V, K>{
+	name: T;
+	login: V;
+	password: K;
+	constructor (
+		name: T,
+		login: V,
+		password: K,
+	) {
+		this.name = name;
+		this.login = login;
+		this.password = password;
+	}
+	createAccount(): void {
+		console.log(`Hi, ${this.name}`);
+	}
+}
+
+const NewPerson = new Person('Tanja', 'login', 'password');
+NewPerson.createAccount();
