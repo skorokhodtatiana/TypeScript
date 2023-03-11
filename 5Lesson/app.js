79150de6b139c37f6b70b673da89f127f8cc1793
tskorohod <tskorohod@lmteam.by>
2023-03-11 21:47:16 +0300

    done task 5 lesson using instanceof

--- 5Lesson/app.js
+++ 5Lesson/app.js
@@ -1,4 +1,4 @@
-//Написать дженерик функцию с одним параметром и с её помощью вывести в консоль сумму чисел,
+//1 Написать дженерик функцию с одним параметром и с её помощью вывести в консоль сумму чисел,
 //если параметры типа number и приветствие, если параметры типа string или boolean.
 function echo(val) {
     if (typeof val === 'number') {
@@ -6,12 +6,12 @@ function echo(val) {
         return val;
     }
     else {
-        console.log('Hy ' + val);
+        console.log('Hi ' + val);
         return val;
     }
 }
 echo("nnnn");
-//Создать обобщённый класс с тремя параметрами (T, V, K).
+//2 Создать обобщённый класс с тремя параметрами (T, V, K).
 var Person = /** @class */ (function () {
     function Person(name, login, password) {
         this.name = name;
@@ -25,3 +25,64 @@ var Person = /** @class */ (function () {
 }());
 var NewPerson = new Person('Tanja', 'login', 'password');
 NewPerson.createAccount();
+//3 Класс содержит три переменные типа (T, V, K), конструктор, принимающий на вход параметры типа (T, V, K), методы,
+//возвращающие значения трёх переменных. Создать метод, выводящий на консоль имена классов для трёх переменных класса.
+var ThreeMetod = /** @class */ (function () {
+    function ThreeMetod(first, second, third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+    ThreeMetod.prototype.firstMethod = function () {
+        return this.first;
+    };
+    ThreeMetod.prototype.secondMethod = function () {
+        return this.second;
+    };
+    ThreeMetod.prototype.thirdMethod = function () {
+        return this.third;
+    };
+    ThreeMetod.prototype.showTypes = function () {
+        console.log(typeof this.first);
+        console.log(typeof this.second);
+        console.log(typeof this.third);
+    };
+    return ThreeMetod;
+}());
+var obj = {
+    name: 'Tanja',
+    fly: function () {
+        console.log('летает');
+    }
+};
+var firs = new ThreeMetod(obj, 28, 'role');
+firs.showTypes();
+console.log(firs.firstMethod());
+console.log(firs.secondMethod());
+console.log(firs.thirdMethod());
+//4 Создать класс Fruits, Vegetables и функцию, которая будет проверять тип переменной
+//и выводить в консоль разный текст, в зависимости от типа данных.
+var Fruits = /** @class */ (function () {
+    function Fruits(fruits) {
+        this.fruits = fruits;
+    }
+    return Fruits;
+}());
+var Vegetables = /** @class */ (function () {
+    function Vegetables(vegetables) {
+        this.vegetables = vegetables;
+    }
+    return Vegetables;
+}());
+function checkType(arg) {
+    if (arg instanceof Fruits) {
+        console.log("".concat(arg.fruits, " \u044D\u0442\u0438 \u0444\u0440\u0443\u043A\u0442\u044B \u043E\u0447\u0435\u043D\u044C \u0441\u043B\u0430\u0434\u043A\u0438\u0435"));
+    }
+    else {
+        console.log("".concat(arg.vegetables, " \u044D\u0442\u0438 \u043E\u0432\u043E\u0449\u0438 \u043E\u0447\u0435\u043D\u044C \u043F\u043E\u043B\u0435\u0437\u043D\u044B\u0435"));
+    }
+}
+var fruit = new Fruits(['яблоко', 'груша', 'апельсин']);
+var veget = new Vegetables(['морковь', 'свекла', 'картофель']);
+checkType(fruit);
+checkType(veget);
--- 5Lesson/app.ts
+++ 5Lesson/app.ts
@@ -1,4 +1,4 @@
-//Написать дженерик функцию с одним параметром и с её помощью вывести в консоль сумму чисел,
+//1 Написать дженерик функцию с одним параметром и с её помощью вывести в консоль сумму чисел,
 //если параметры типа number и приветствие, если параметры типа string или boolean.
 function echo <T>(val: T): T {
 	if (typeof val === 'number') {
@@ -12,7 +12,7 @@ function echo <T>(val: T): T {
 
 echo("nnnn");
 
-//Создать обобщённый класс с тремя параметрами (T, V, K).
+//2 Создать обобщённый класс с тремя параметрами (T, V, K).
 class Person <T, V, K> {
 	name: T;
 	login: V;
@@ -34,7 +34,7 @@ class Person <T, V, K> {
 const NewPerson = new Person('Tanja', 'login', 'password');
 NewPerson.createAccount();
 
-//Класс содержит три переменные типа (T, V, K), конструктор, принимающий на вход параметры типа (T, V, K), методы,
+//3 Класс содержит три переменные типа (T, V, K), конструктор, принимающий на вход параметры типа (T, V, K), методы,
 //возвращающие значения трёх переменных. Создать метод, выводящий на консоль имена классов для трёх переменных класса.
 
 class ThreeMetod <T, V, K>{
@@ -46,12 +46,65 @@ class ThreeMetod <T, V, K>{
 		this.second = second;
 		this.third = third;
 	}
-	firstMetod() {
-		if (typeof this.first !== undefined ) {
-			console.log(typeof this.first)
-		} else {
-			console.log(instanceof this.first)
-		}
-		
+	firstMethod(): T {
+		return this.first;
+	}
+
+	secondMethod(): V {
+		return this.second;
+	}
+
+	thirdMethod(): K {
+		return this.third;
+	}
+
+	showTypes():void {
+		console.log(typeof this.first);
+		console.log(typeof this.second);
+		console.log(typeof this.third);
 	}
 }
+
+const obj = {
+	name: 'Tanja',
+	fly() {
+		console.log('летает');
+	}
+}
+
+const firs = new ThreeMetod(obj, 28, 'role');
+
+firs.showTypes();
+console.log(firs.firstMethod());
+console.log(firs.secondMethod());
+console.log(firs.thirdMethod());
+
+//4 Создать класс Fruits, Vegetables и функцию, которая будет проверять тип переменной
+//и выводить в консоль разный текст, в зависимости от типа данных.
+class Fruits {
+	fruits: Array<string>;
+	constructor(fruits: Array<string>) {
+		this.fruits = fruits
+	}
+}
+
+class Vegetables {
+	vegetables: Array<string>;
+	constructor(vegetables: Array<string>) {
+		this.vegetables = vegetables
+	}
+}
+
+function checkType (arg: Fruits | Vegetables) {
+	if (arg instanceof Fruits) {
+		console.log(`${arg.fruits} эти фрукты очень сладкие`);
+	} else {
+		console.log(`${arg.vegetables} эти овощи очень полезные`);
+	}
+}
+
+const fruit = new Fruits(['яблоко', 'груша', 'апельсин']);
+const veget = new Vegetables(['морковь', 'свекла', 'картофель']);
+checkType(fruit);
+checkType(veget);
+
