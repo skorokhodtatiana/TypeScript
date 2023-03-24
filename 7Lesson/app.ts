c072fbf8b54fd1e8211d1cdaccc8ff52de564323
tskorohod <tskorohod@lmteam.by>
2023-03-24 21:50:54 +0300

    hw 7 lesson, solid principles

---
+++ 7Lesson/app.ts
@@ -0,0 +1,11 @@
+class Cart {
+	name: String;
+	price: Number;
+	num: Number;
+
+	constructor (name: String, price: Number, num: Number) {
+		this.name =name;
+		this.price = price;
+		this.num = num;
+	}
+}
